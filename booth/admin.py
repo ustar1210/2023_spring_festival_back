ab8b6fe3ed2586e154b0ca1a935e297e12744525
tkdwns414 <sangjune2000@naver.com>
2023-05-16 03:50:14 +0900

    똥을 치웠습니다.

--- booth/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Booth)
-admin.site.register(Like)
-admin.site.register(BaseImage)
-admin.site.register(MenuImage)
-admin.site.register(LogoImage)
-admin.site.register(NotificationImage)
