b14011093725b719f2e2d67fac245b9f32e83179
jenny <88226911+kmjenny@users.noreply.github.com>
2023-05-16 05:11:58 +0900

    feature/2: Edit urls

--- project/urls.py
+++ project/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('notice.urls')),
+    path("api/", include('notice.urls')),
 ] + static(settings.MEDIA_URL, docnument_root=settings.MEDIA_ROOT)
